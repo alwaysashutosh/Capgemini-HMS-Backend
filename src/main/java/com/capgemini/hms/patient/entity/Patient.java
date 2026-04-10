b7ec3f73d5888c6b2e9d0e43c1f528ac1de52803
Rahul <mouryarahul543@gmail.com>
2026-04-10 14:32:40 +0530

    Update patient entity with insurance ID and soft delete field.

--- src/main/java/com/capgemini/hms/patient/entity/Patient.java
+++ src/main/java/com/capgemini/hms/patient/entity/Patient.java
@@ -1,25 +1,10 @@
 package com.capgemini.hms.patient.entity;
 
 import com.capgemini.hms.physician.entity.Physician;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "patient")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Patient {
 
     @Id
@@ -36,9 +21,80 @@ public class Patient {
     private String phone;
 
     @Column(name = "insuranceid", nullable = false)
-    private Integer insuranceId;
+    private String insuranceId;
 
     @ManyToOne
     @JoinColumn(name = "pcp", referencedColumnName = "employeeid")
     private Physician pcp;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted = false;
+
+    public Patient() {
+    }
+
+    public Patient(Integer ssn, String name, String address, String phone, String insuranceId, Physician pcp) {
+        this.ssn = ssn;
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.insuranceId = insuranceId;
+        this.pcp = pcp;
+    }
+
+    public Integer getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(Integer ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getInsuranceId() {
+        return insuranceId;
+    }
+
+    public void setInsuranceId(String insuranceId) {
+        this.insuranceId = insuranceId;
+    }
+
+    public Physician getPcp() {
+        return pcp;
+    }
+
+    public void setPcp(Physician pcp) {
+        this.pcp = pcp;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
 }
