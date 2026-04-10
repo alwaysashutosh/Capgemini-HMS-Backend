c41ad3a0a0026f79bc3365b3c8270db39f51dc53
Ayush yede <ayushyedestudy24@gmail.com>
2026-04-10 15:45:06 +0530

    added room entity

--- src/main/java/com/capgemini/hms/room/entity/Room.java
+++ src/main/java/com/capgemini/hms/room/entity/Room.java
@@ -7,19 +7,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinColumns;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "room")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Room {
 
     @Id
@@ -38,4 +28,57 @@ public class Room {
 
     @Column(name = "unavailable", nullable = false)
     private Boolean unavailable;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted = false;
+
+    public Room() {
+    }
+
+    public Room(Integer roomNumber, String roomType, Block block, Boolean unavailable) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.block = block;
+        this.unavailable = unavailable;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public Block getBlock() {
+        return block;
+    }
+
+    public void setBlock(Block block) {
+        this.block = block;
+    }
+
+    public Boolean getUnavailable() {
+        return unavailable;
+    }
+
+    public void setUnavailable(Boolean unavailable) {
+        this.unavailable = unavailable;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
 }
